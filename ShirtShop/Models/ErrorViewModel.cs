d5a224ab642147e49c3d4fbc81de7bd6b6d3b6f3
Marcin <marcinnns95@gmail.com>
2021-11-28 16:00:55 +0100

    First commit

---
+++ ShirtShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShirtShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
